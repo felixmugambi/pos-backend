8572b51ffaab1c6e53542a0435f240f8b3982e09
RESRAS SILEN <mugafelhida98@gmail.com>
2026-04-29 05:47:58 -0700

    cors update

--- src/app.js
+++ src/app.js
@@ -11,31 +11,14 @@ import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
-app.use((req, res, next) => {
-    const origin = req.headers.origin;
-  
-    const allowedOrigins = [
+app.use(cors({
+    origin: [
       'http://localhost:3000',
       'http://localhost:5173',
       'https://pos-frontend-seven-brown.vercel.app'
-    ];
-  
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    } else {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-    }
-  
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-    if (req.method === 'OPTIONS') {
-      return res.status(204).end();
-    }
-  
-    next();
-  });
+    ],
+    credentials: true
+  }));
   
 app.use(express.json());
 
